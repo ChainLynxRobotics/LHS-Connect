0d8e7258948d1ba69192718d3873700a3edbb973
Tyler Place <79611123+1withspaghetti@users.noreply.github.com>
2024-09-02 22:06:43 -0700

    FIXED THAT STUPID RERENDERING LOOP BUG I CAN FINALLY DIE HAPPY

--- src/routes/(admin)/admin/panel/home/bell-schedule/EditScheduleForm.svelte
+++ src/routes/(admin)/admin/panel/home/bell-schedule/EditScheduleForm.svelte
@@ -1,64 +1,68 @@
 <script lang="ts">
 	import ValidatedInput from "$components/form/ValidatedInput.svelte";
-    import type { BellSchedule, TimeString } from "$lib/types/HomePageData";
+    import type { BellSchedule } from "$lib/types/HomePageData";
     import bellScheduleSchema, { bellSchedulePeriodSchemaName } from "$lib/schemas/bellScheduleSchema";
 	import ValidatedTextarea from "$components/form/ValidatedTextarea.svelte";
 	import { Button, Table, TableBodyCell, TableHead, TableHeadCell } from "flowbite-svelte";
-	import DraggableList from "$components/admin/DraggableList.svelte";
 	import { createEventDispatcher } from "svelte";
-	import { dragHandle } from "svelte-dnd-action";
+	import { dragHandle, dragHandleZone } from "svelte-dnd-action";
 	import DragHandleOutline from "$components/admin/DragHandleOutline.svelte";
     import { FileCopyOutline, TrashBinOutline } from "flowbite-svelte-icons";
 	import CheckboxWithAccessors from "$components/form/CheckboxWithAccessors.svelte";
+	import { flip } from "svelte/animate";
 
+    const flipDurationMs = 300;
 
     export let schedule: BellSchedule;
-    $: schedule = JSON.parse(JSON.stringify(schedule)); // Deep copy
     export let onSave: (schedule: BellSchedule) => void;
 
     const dispatch = createEventDispatcher<{
         close: null;
     }>();
 
+    let name = schedule.name;
+    let desc = schedule.desc;
+    let periods = schedule.periods.map((period) => ({...period, id: Math.random()}));
+
     let nameInput: ValidatedInput<'name'>;
     let descInput: ValidatedTextarea<'desc'>;
-    let periodNames: ValidatedInput<string>[] = [];
-    let periodStarts: HTMLInputElement[] = [];
-    let periodEnds: HTMLInputElement[] = [];
-    let periodEmphasizes: CheckboxWithAccessors[] = [];
+    let periodNameInputs: ValidatedInput<string>[] = [];
+    
 
     function handleAddPeriod() {
-        schedule.periods.push({name: 'Period', start: '00:00', end: '00:00', emphasis: false});
-        schedule = schedule; // Force update
+        periods.push({name: 'Period', start: '00:00', end: '00:00', emphasis: false, id: Math.random()});
+        periods = periods; // Force update
     }
 
     function handleDuplicatePeriod(index: number) {
-        schedule.periods.splice(index, 0, JSON.parse(JSON.stringify(schedule.periods[index])));
-        schedule = schedule; // Force update
+        periods.splice(index, 0, {...periods[index], id: Math.random()});
+        periods = periods; // Force update
     }   
 
     function handleDeletePeriod(index: number) {
-        schedule.periods.splice(index, 1);
-        schedule = schedule; // Force update
+        periods.splice(index, 1);
+        periods = periods; // Force update
     }
 
     async function handleSave() {
         if (![
-            ...(await Promise.all(periodNames.map((input) => input.validate(true)))),
+            ...(await Promise.all(periodNameInputs.map((input) => input.validate(true)))),
             await nameInput.validate(true), await descInput.validate(true)
             // Start times and end times are validated by the browser
         ].every(v=>v)) return; // If one of the validations failed, don't save
 
         onSave({
-            name: nameInput.value||'',
-            desc: descInput.value,
-            periods: schedule.periods.map((period, i) => ({
-                name: periodNames[i].value||'',
-                start: periodStarts[i].value as TimeString,
-                end: periodEnds[i].value as TimeString,
-                emphasis: periodEmphasizes[i].checked
-            }))
+            name,
+            desc,
+            periods: periods.map((period) => ({...period, id: undefined}))
         });
+    }
+
+    function handleDragConsider(event: CustomEvent<{items: any[]}>) {
+        periods = event.detail.items;
+    }
+    function handleDragFinalize(event: CustomEvent<{items: any[]}>) {
+        periods = event.detail.items;
     }
 </script>
 
@@ -71,7 +75,7 @@
             bind:this={nameInput} 
             id="name" 
             label="Name" 
-            value={schedule.name} 
+            bind:value={name} 
             validatorObject={bellScheduleSchema}
             visuallyRequired
         />
@@ -81,7 +85,7 @@
             bind:this={descInput}
             id="desc"
             label="Description"
-            value={schedule.desc}
+            bind:value={desc}
             validatorObject={bellScheduleSchema}
         />
     </div>
@@ -96,71 +100,66 @@
                 <TableHeadCell>Bold</TableHeadCell>
                 <TableHeadCell>Actions</TableHeadCell>
             </TableHead>
-            <DraggableList 
-                dragZoneType="periods"
-                items={schedule.periods} 
-                update={(periods)=>schedule.periods = periods} 
-                let:item 
-                let:index
-                sectionElement="tbody"
-                sectionClass="divide-y text-start"
-                dragWrapperElement="tr"
-                dragWrapperClass="group border-b last:border-b-0 bg-white dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700"
+            <tbody
+                use:dragHandleZone={{type: 'periods', items: periods, flipDurationMs}}
+                on:consider={handleDragConsider}
+                on:finalize={handleDragFinalize}
             >
-                <td use:dragHandle class="pl-6 py-4">
-                    <div class="flex items-center">
-                        <DragHandleOutline />
-                    </div>
-                </td>
-                <TableBodyCell tdClass="min-w-48 px-6 py-4 whitespace-nowrap font-medium">
-                    <ValidatedInput
-                        bind:this={periodNames[index]}
-                        id={`period-${index}-name`}
-                        value={schedule.periods[index].name}
-                        validator={bellSchedulePeriodSchemaName}
-                        visuallyRequired
-                    />
-                </TableBodyCell>
-                <TableBodyCell>
-                    <input 
-                        bind:this={periodStarts[index]}
-                        id={`period-${index}-start`}
-                        name={`period-${index}-start`}
-                        type="time"
-                        aria-label="Start Time"
-                        value={schedule.periods[index].start}
-                        class="border-0 ring-primary-500 bg-white dark:bg-gray-800 dark:border-gray-700 group-odd:bg-white group-even:bg-gray-50 group-odd:dark:bg-gray-800 group-even:dark:bg-gray-700"
-                    />
-                </TableBodyCell>
-                <TableBodyCell>
-                    <input 
-                        bind:this={periodEnds[index]}
-                        id={`period-${index}-end`}
-                        name={`period-${index}-end`}
-                        type="time"
-                        aria-label="End Time"
-                        value={schedule.periods[index].end}
-                        class="border-0 ring-primary-500 bg-white dark:bg-gray-800 dark:border-gray-700 group-odd:bg-white group-even:bg-gray-50 group-odd:dark:bg-gray-800 group-even:dark:bg-gray-700"
-                    />
-                </TableBodyCell>
-                <TableBodyCell>
-                    <CheckboxWithAccessors
-                        bind:this={periodEmphasizes[index]}
-                        checkboxProps={{
-                            id: `period-${index}-emphasize`,
-                            name: `period-${index}-emphasize`,
-                            'aria-label': 'Emphasize'
-                        }}
-                        checked={schedule.periods[index].emphasis}
-                    />
-                </TableBodyCell>
-                <TableBodyCell>
-                    <div class="flex">
-                        <button type="button" on:click={()=>handleDuplicatePeriod(index)} class="!p-2"><FileCopyOutline class="h-6 w-6" /></button>
-                        <button type="button" on:click={()=>handleDeletePeriod(index)} class="!p-2"><TrashBinOutline class="h-6 w-6 text-red-500 dark:text-red-400" /></button>
-                    </div>
-                </TableBodyCell>
-            </DraggableList>
+                {#each periods as period, index (period.id)}
+                    <tr animate:flip={{duration: flipDurationMs}} class="group border-b last:border-b-0 bg-white dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                        <td use:dragHandle class="pl-6 py-4">
+                            <div class="flex items-center">
+                                <DragHandleOutline />
+                            </div>
+                        </td>
+                        <TableBodyCell tdClass="min-w-48 px-6 py-4 whitespace-nowrap font-medium">
+                            <ValidatedInput
+                                bind:this={periodNameInputs[index]}
+                                id={`period-${index}-name`}
+                                bind:value={period.name}
+                                validator={bellSchedulePeriodSchemaName}
+                                visuallyRequired
+                            />
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <input 
+                                id={`period-${index}-start`}
+                                name={`period-${index}-start`}
+                                type="time"
+                                aria-label="Start Time"
+                                bind:value={period.start}
+                                class="border-0 ring-primary-500 bg-white dark:bg-gray-800 dark:border-gray-700 group-odd:bg-white group-even:bg-gray-50 group-odd:dark:bg-gray-800 group-even:dark:bg-gray-700"
+                            />
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <input 
+                                id={`period-${index}-end`}
+                                name={`period-${index}-end`}
+                                type="time"
+                                aria-label="End Time"
+                                bind:value={period.end}
+                                class="border-0 ring-primary-500 bg-white dark:bg-gray-800 dark:border-gray-700 group-odd:bg-white group-even:bg-gray-50 group-odd:dark:bg-gray-800 group-even:dark:bg-gray-700"
+                            />
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <CheckboxWithAccessors
+                                checkboxProps={{
+                                    id: `period-${index}-emphasize`,
+                                    name: `period-${index}-emphasize`,
+                                    'aria-label': 'Emphasize'
+                                }}
+                                bind:checked={period.emphasis}
+                            />
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <div class="flex">
+                                <button type="button" on:click={()=>handleDuplicatePeriod(index)} class="!p-2"><FileCopyOutline class="h-6 w-6" /></button>
+                                <button type="button" on:click={()=>handleDeletePeriod(index)} class="!p-2"><TrashBinOutline class="h-6 w-6 text-red-500 dark:text-red-400" /></button>
+                            </div>
+                        </TableBodyCell>
+                    </tr>
+                {/each}
+            </tbody>
         </Table>
     </div>
 
