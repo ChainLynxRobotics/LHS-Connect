65e70cafafacb2d31d9e8ba94ab7a5d95bfe5b74
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2024-11-04 14:24:55 -0800

    Page transitions

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-	import type { GlobalPageData } from '$lib/types/GlobalPageData';
+	import { cubicIn, cubicOut } from 'svelte/easing';
 	import Footer from './Footer.svelte';
 	import Header from './Header.svelte';
 	import QuickLinks from './QuickLinks.svelte';
+	import { fly } from 'svelte/transition';
+	import type { LayoutData } from './$types';
 
-	export let data: GlobalPageData;
+	export let data: LayoutData;
+
+	// Page transitions
+	const duration = 200;
+	const delay = duration + 50;
+	const y = 10;
+
+	const transitionIn = { easing: cubicOut, y, duration, delay };
+	const transitionOut = { easing: cubicIn, y: -y, duration };
 </script>
 
 <div class="min-h-screen bg-white dark:bg-gray-900">
@@ -13,6 +23,10 @@
 		<QuickLinks />
 	</div>
 
-	<slot></slot>
+	{#key data.pathname}
+		<div in:fly={transitionIn} out:fly={transitionOut}>
+			<slot />
+		</div>
+	{/key}
 </div>
 <Footer data={data.footer}></Footer>
---
+++ src/routes/(public)/+layout.ts
@@ -0,0 +1,8 @@
+import type { LayoutLoad } from './$types';
+
+export const load = (async ({ url, data }) => {
+    return {
+        ...data,
+        pathname: url.pathname,
+    };
+}) satisfies LayoutLoad;
