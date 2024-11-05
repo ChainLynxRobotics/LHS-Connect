f078c8d7f418dbe69b1358ce560b9c70d966d9d0
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2024-11-04 17:48:13 -0800

    Error pages

---
+++ src/lib/components/ErrorCard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { A, Button, Card } from "flowbite-svelte";
+	import { ArrowRightOutline } from "flowbite-svelte-icons";
+
+    export let status: number;
+    export let error: App.Error|null;
+</script>
+
+<Card size="md">
+    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Oh my goodness gracious...</h5>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+        You've encountered an error! If this is unexpected, please <A href="/about?type=bug#contact">contact us</A>!
+    </p>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+        Status: <code>{status}</code>
+        {#if error?.message}
+            <br />
+            Message: <code>{error.message}</code>
+        {/if}
+    </p>
+    <Button href="/" class="w-fit mt-4">
+        Return to Home <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
+    </Button>
+</Card>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+	import ErrorCard from '$components/ErrorCard.svelte';
+</script>
+
+<svelte:head>
+	<title>LHS Connect | Error</title>
+</svelte:head>
+
+<div class="flex w-full flex-col flex-wrap items-center p-4 pt-16 pb-16">
+	<ErrorCard status={$page.status} error={$page.error} />
+</div>
