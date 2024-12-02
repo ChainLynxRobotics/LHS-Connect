87b9673b759eceb9249d23aaf952b492615c161e
Tyler Place <79611123+1withspaghetti@users.noreply.github.com>
2024-12-01 17:33:01 -0800

    Removal of fake data

--- src/routes/admin/panel/links/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-// TODO: Replace this with actual data loading.
-import shortLinks from '$lib/fake_data/shortLinks';
-
-export const load = (async () => {
-	return shortLinks;
-}) satisfies PageServerLoad;
