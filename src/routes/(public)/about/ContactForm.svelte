aee9b84e3e15e4b8101beb21c4a7dbbb282bad2d
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-06-10 11:04:28 -0700

    format

--- src/routes/(public)/about/ContactForm.svelte
+++ src/routes/(public)/about/ContactForm.svelte
@@ -106,7 +106,9 @@
 			validatorObject={contactUsValidator}
 			textareaProps={{ placeholder: 'Your message', rows: 4 }}
 		/>
-			<p class="text-sm text-gray-400 italic text-center">Please do not abuse this form. LHS Connect is built and maintained by students.</p>
+		<p class="text-center text-sm italic text-gray-400">
+			Please do not abuse this form. LHS Connect is built and maintained by students.
+		</p>
 	</div>
 	<div class="flex justify-center">
 		<Button type="submit">
