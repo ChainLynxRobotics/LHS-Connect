9aaf17cc8c695e2d066b0d8021f8fe3c878d7b23
Elliott Weston Ball <elliottwball@gmail.com>
2026-06-09 23:21:28 -0700

    change extra page link to ap stats project from button to hyperlink

--- src/routes/(public)/extra/+page.svelte
+++ src/routes/(public)/extra/+page.svelte
@@ -1,17 +1,8 @@
 <script lang="ts">
 	import SectionHeader from '$components/SectionHeader.svelte';
-	import { Button, Card, Modal } from 'flowbite-svelte';
+	import { Button, Card} from 'flowbite-svelte';
 
-	import { DiscordSolid, InstagramSolid, GithubSolid, MailBoxOutline, StarOutline } from 'flowbite-svelte-icons';
-	import ExternalLink from '$components/ExternalLink.svelte';
-
-	let copyTextModal = $state(false);
-	let copyTextContent = $state('');
-	function copyText(text: string) {
-		navigator.clipboard.writeText(text);
-		copyTextContent = text;
-		copyTextModal = true;
-	}
+	import GithubSolid from 'flowbite-svelte-icons';
 </script>
 
 <svelte:head>
@@ -24,23 +15,13 @@
 		<div class="mx-auto flex flex-wrap justify-center gap-4">
 			<Card padding="md" size="xs">
 				<div class="flex flex-col items-center">
-					<h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">AP Stats Practice</h5>
+					<a href="/extra/ap_stats_inference_practice"><h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">AP Stats Practice</h5></a>
 					<span class="text-gray-500 dark:text-gray-400">A Project By <a href="/about#team">Tyler Place</a></span>
 					<p class="intent-8 mt-4 text-center">
 						This site is an practice tool to teach Inference Procedures for AP Statistics. 
 						The user identifies the correct inference procedure for a given practice problem, and is scored on accuracy.
 					</p>
 					<div class="mt-4 flex gap-4">
-						<Button
-							href="/extra/ap_stats_inference_practice"
-							target="_blank"
-							rel="noopener"
-							title="View Project"
-							color="light"
-							class="dark:text-white"
-						>
-							<StarOutline size="lg" />
-						</Button>
 						<Button
 							href="https://github.com/1withspaghetti/AP-Stats-Inference-Practice"
 							target="_blank"
