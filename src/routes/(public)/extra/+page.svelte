24b5d1a323740f2cf200178ed64c2c498391d6ba
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-06-11 11:18:21 -0700

    format (#23)
    Clean up code formatting

--- src/routes/(public)/extra/+page.svelte
+++ src/routes/(public)/extra/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import SectionHeader from '$components/SectionHeader.svelte';
-	import { Button, Card} from 'flowbite-svelte';
+	import { Button, Card } from 'flowbite-svelte';
 	import ExternalLink from '$components/ExternalLink.svelte';
-	import { GithubSolid} from 'flowbite-svelte-icons';
+	import { GithubSolid } from 'flowbite-svelte-icons';
 </script>
 
 <svelte:head>
@@ -16,15 +16,15 @@
 			<Card padding="md" size="xs">
 				<div class="flex flex-col items-center">
 					<h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-						<ExternalLink
-							text="AP Stats Practice"
-							href="/extra/ap_stats_inference_practice"
-						/>
+						<ExternalLink text="AP Stats Practice" href="/extra/ap_stats_inference_practice" />
 					</h5>
-					<span class="text-gray-500 dark:text-gray-400">A Project By <a href="/about#team">Tyler Place</a></span>
+					<span class="text-gray-500 dark:text-gray-400"
+						>A Project By <a href="/about#team">Tyler Place</a></span
+					>
 					<p class="intent-8 mt-4 text-center">
-						This site is a practice tool to teach Inference Procedures for AP Statistics. 
-						The user identifies the correct inference procedure for a given practice problem, and is scored on accuracy.
+						This site is a practice tool to teach Inference Procedures for AP Statistics. The user
+						identifies the correct inference procedure for a given practice problem, and is scored
+						on accuracy.
 					</p>
 					<div class="mt-4 flex gap-4">
 						<Button
